a2da199214d86416d1c8959a0f7b67d1162f7077
erdaltsksn <erdal@taskesen.me>
2020-08-25 17:28:16 +0300

    redo: (letter) Fix the warning for string(x)
    why: The vet tool now warns about conversions of the form string(x)
         where x has an integer type other than rune or byte
    
    see: https://golang.org/doc/go1.15#vet

--- letter.go
+++ letter.go
@@ -11,19 +11,19 @@ func Letter() string {
 	rand.Seed(time.Now().UnixNano())
 	r := rand.Intn(2)
 
-	return string(letterCase[r] + rand.Intn(26))
+	return string(rune(letterCase[r] + rand.Intn(26)))
 }
 
 // LetterLowerCase generate a random lowercase letter.
 func LetterLowerCase() string {
 	rand.Seed(time.Now().UnixNano())
 
-	return string('a' + rand.Intn(26))
+	return string(rune('a' + rand.Intn(26)))
 }
 
 // LetterUpperCase generate a random uppercase letter.
 func LetterUpperCase() string {
 	rand.Seed(time.Now().UnixNano())
 
-	return string('A' + rand.Intn(26))
+	return string(rune('A' + rand.Intn(26)))
 }
