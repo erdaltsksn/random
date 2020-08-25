8f838a5a9cf7cb1561b427eeecf8c75f81c5ce0e
erdaltsksn <erdal@taskesen.me>
2020-08-25 14:59:48 +0300

    feat: (quote) Add more quotes

--- quote_data.go
+++ quote_data.go
@@ -127,4 +127,108 @@ Walk beside me… just be my friend`,
 		Text:   `Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.`,
 		Author: "H. Jackson Brown Jr.",
 	},
+	{
+		Text:   `So, this is my life. And I want you to know that I am both happy and sad and I'm still trying to figure out how that could be.`,
+		Author: "Stephen Chbosky",
+	},
+	{
+		Text:   `You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.`,
+		Author: "John Lennon",
+	},
+	{
+		Text:   `And, when you want something, all the universe conspires in helping you to achieve it.`,
+		Author: "Paulo Coelho",
+	},
+	{
+		Text:   `It’s no use going back to yesterday, because I was a different person then.`,
+		Author: "Lewis Carroll",
+	},
+	{
+		Text:   `What you're supposed to do when you don't like a thing is change it. If you can't change it, change the way you think about it. Don't complain.`,
+		Author: "Maya Angelou",
+	},
+	{
+		Text:   `It's the possibility of having a dream come true that makes life interesting.`,
+		Author: "Paulo Coelho",
+	},
+	{
+		Text:   `A person's a person, no matter how small.`,
+		Author: "Dr. Seuss",
+	},
+	{
+		Text:   `He’s not perfect. You aren’t either, and the two of you will never be perfect. But if he can make you laugh at least once, causes you to think twice, and if he admits to being human and making mistakes, hold onto him and give him the most you can. He isn’t going to quote poetry, he’s not thinking about you every moment, but he will give you a part of him that he knows you could break. Don’t hurt him, don’t change him, and don’t expect for more than he can give. Don’t analyze. Smile when he makes you happy, yell when he makes you mad, and miss him when he’s not there. Love hard when there is love to be had. Because perfect guys don’t exist, but there’s always one guy that is perfect for you.`,
+		Author: "Bob Marley",
+	},
+	{
+		Text:   `You can't live your life for other people. You've got to do what's right for you, even if it hurts some people you love.`,
+		Author: "Nicholas Sparks",
+	},
+	{
+		Text:   `Well-behaved women seldom make history.`,
+		Author: "Laurel Thatcher Ulrich",
+	},
+	{
+		Text:   `When I despair, I remember that all through history the way of truth and love have always won. There have been tyrants and murderers, and for a time, they can seem invincible, but in the end, they always fall. Think of it--always.`,
+		Author: "Mahatma Gandhi",
+	},
+	{
+		Text:   `So we beat on, boats against the current, borne back ceaselessly into the past.`,
+		Author: "F. Scott Fitzgerald",
+	},
+	{
+		Text:   `Nothing is impossible, the word itself says 'I'm possible'!`,
+		Author: "Audrey Hepburn",
+	},
+	{
+		Text:   `I can't give you a sure-fire formula for success, but I can give you a formula for failure: try to please everybody all the time.`,
+		Author: "Herbert Bayard Swope",
+	},
+	{
+		Text:   `Do what you feel in your heart to be right – for you’ll be criticized anyway.`,
+		Author: "Eleanor Roosevelt",
+	},
+	{
+		Text:   `Happiness is not something ready made. It comes from your own actions.`,
+		Author: "Dalai Lama XIV",
+	},
+	{
+		Text:   `Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has.`,
+		Author: "Margaret Mead",
+	},
+	{
+		Text:   `Peace begins with a smile..`,
+		Author: "Mother Teresa",
+	},
+	{
+		Text:   `Do not read, as children do, to amuse yourself, or like the ambitious, for the purpose of instruction. No, read in order to live.`,
+		Author: "Gustave Flaubert",
+	},
+	{
+		Text:   `What lies behind us and what lies before us are tiny matters compared to what lies within us.`,
+		Author: "Ralph Waldo Emerson",
+	},
+	{
+		Text:   `Friendship is unnecessary, like philosophy, like art.... It has no survival value; rather it is one of those things which give value to survival.`,
+		Author: "C.S. Lewis",
+	},
+	{
+		Text:   `Whatever you are, be a good one.`,
+		Author: "Abraham Lincoln",
+	},
+	{
+		Text:   `I hope she'll be a fool -- that's the best thing a girl can be in this world, a beautiful little fool.`,
+		Author: "F. Scott Fitzgerald",
+	},
+	{
+		Text:   `Two wrongs don't make a right, but they make a good excuse.`,
+		Author: "Thomas Szasz",
+	},
+	{
+		Text:   `First they ignore you. Then they ridicule you. And then they attack you and want to burn you. And then they build monuments to you.`,
+		Author: "Nicholas Klein",
+	},
+	{
+		Text:   `May you live every day of your life.`,
+		Author: "Jonathan Swift",
+	},
 }
